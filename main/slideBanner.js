5c51c019f5cbb360f4899d03af1c2a088fc7e34e
unknown <aiden915y@gmail.com>
2024-01-03 16:08:51 +0900

    2024/01/03

--- main/slideBanner.js
+++ main/slideBanner.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     perMove: 1,
     autoplay: true,
     interval: 3000,
+    speed: 500,
     pagination: false,
     width: "100%",
     padding: "0",
-    trimSpace: true,
+    arrows: false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
   });
   splide.mount();
 });
